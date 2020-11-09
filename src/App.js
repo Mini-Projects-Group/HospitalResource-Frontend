8ba81d0d899f5cccfeae8f5cc372d68b15e6b2c4
Nimish Dadlani <nmd2611@gmail.com>
2020-11-09 12:18:03 +0530

    Sidebar done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Dashboard from "./components/dashboard/Dashboard";
-import PrivateRoute from "./reusables/routes/PrivateRoute";
-import Navbar from "./components/navbar/Navbar";
-import Signin from "./components/signin/Signin";
-import Register from "./components/register/Register";
-import Home from "./components/home/Home";
 import Auth from "./components/auth/Auth";
+import Dashboard from "./components/dashboard/Dashboard";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Register from "./components/register/Register";
 import SellerDashboard from "./components/sellerDashboard/SellerDashboard";
+import Signin from "./components/signin/Signin";
+import PrivateRoute from "./reusables/routes/PrivateRoute";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/sd" component={SellerDashboard} />
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <PrivateRoute exact path="/auth" Component={Auth} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/sd' component={SellerDashboard} />
+          <Route exact path='/signin' component={Signin} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <PrivateRoute exact path='/auth' Component={Auth} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useRef } from "react";
+import styles from "./Sidebar.module.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiLogOutCircle } from "react-icons/bi";
+import { BsListOl } from "react-icons/bs";
+import { useLocation, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { LOGOUT_SUCCESS } from "../../redux";
+
+const Sidebar = () => {
+  const selectedStyle = {
+    main: {
+      backgroundColor: "#1c1e36",
+    },
+
+    textColor: {
+      color: "white",
+    },
+  };
+
+  const myL = useRef("/dashboard");
+  const selected = useRef(0);
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch({ type: LOGOUT_SUCCESS });
+  };
+
+  myL.current = useLocation().pathname;
+
+  switch (myL.current) {
+    case "/dashboard":
+      selected.current = 0;
+      break;
+
+    case "/orders":
+      selected.current = 1;
+      break;
+
+    case "/algorithm":
+      selected.current = 3;
+      break;
+
+    case "/logout":
+      selected.current = 4;
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <div className={styles.root}>
+      <Link
+        to='/dashboard'
+        className={styles.main}
+        style={selected.current === 0 ? selectedStyle.main : null}
+      >
+        <div
+          className={styles.iconDiv}
+          style={selected.current === 0 ? selectedStyle.textColor : null}
+        >
+          <AiOutlineHome size={35} />
+        </div>
+        <div
+          className={styles.textDiv}
+          style={selected.current === 0 ? selectedStyle.textColor : null}
+        >
+          Dashboard
+        </div>
+      </Link>
+
+      <Link
+        to='/orders'
+        className={styles.main}
+        style={selected.current === 1 ? selectedStyle.main : null}
+      >
+        <div
+          className={styles.iconDiv}
+          style={selected.current === 1 ? selectedStyle.textColor : null}
+        >
+          <BsListOl size={35} />
+        </div>
+        <div
+          className={styles.textDiv}
+          style={selected.current === 1 ? selectedStyle.textColor : null}
+        >
+          Orders
+        </div>
+      </Link>
+
+      <Link
+        to='/logout'
+        className={`${styles.main} ${styles.logout}`}
+        style={selected.current === 4 ? selectedStyle.main : null}
+        onClick={handleLogout}
+      >
+        <div
+          className={styles.iconDiv}
+          style={selected.current === 4 ? selectedStyle.textColor : null}
+        >
+          <BiLogOutCircle size={35} />
+        </div>
+        <div
+          className={styles.textDiv}
+          style={selected.current === 4 ? selectedStyle.textColor : null}
+        >
+          Log Out
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Sidebar;
