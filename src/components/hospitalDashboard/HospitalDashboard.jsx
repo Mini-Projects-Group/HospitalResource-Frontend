350d2706622b7000cb8e44914c676f4d2b04a83f
Nimish Dadlani <nmd2611@gmail.com>
2020-11-18 12:27:30 +0530

    basic Pie chart added

--- src/components/hospitalDashboard/HospitalDashboard.jsx
+++ src/components/hospitalDashboard/HospitalDashboard.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./HospitalDashboard.module.css";
+import { async_func_data } from "../../redux";
+import { BAD_STATUS } from "../../redux/utils/constants";
+import PieChart from "../../reusables/components/piechart/PieChart";
 
 const HospitalDashboard = () => {
-  return <div className={styles.root}>Hospital Dashboard</div>;
+  const [data, setData] = useState([]);
+  const [stock_items, setStockItems] = useState([]);
+  const [stock, setStock] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  let temp = [];
+
+  useEffect(() => {
+    const f = async () => {
+      setLoading(true);
+      try {
+        let res;
+
+        res = await async_func_data("api/stock", null, "get", true);
+        console.log(res);
+        if (res.status !== BAD_STATUS) {
+          setStockItems(res.data.stock_items);
+          setStock(res.data.stock);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+
+      setLoading(false);
+    };
+    f();
+  }, []);
+
+  let i, j;
+
+  for (i = 0; i < stock.length; i++) {
+    temp.push({
+      id: stock[i].item_id,
+      label: `${stock[i].item} : ${stock_items[i].quantity}`,
+      value: parseFloat(stock[i].percent),
+      //color: "hsl(111, 70%, 50%)",
+      // color: `hsl(${Math.random() * 1000}, ${parseInt(stock[i].percent)}% , ${
+      //   110 - parseInt(stock[i].percent)
+      // }%)`,
+    });
+  }
+
+  // setData(temp);
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.pieCard}>
+        <PieChart data={temp} />
+      </div>
+    </div>
+  );
 };
 
 export default HospitalDashboard;
